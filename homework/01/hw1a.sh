333326441cec3cc84b5ecb8bcdd1da5c62dda802
Henry Post <HenryFBP@gmail.com>
2019-08-27 18:19:02 -0500

    show os release

--- homework/01/hw1a.sh
+++ homework/01/hw1a.sh
@@ -6,6 +6,12 @@ function get_ips {
     echo "$(ifconfig | egrep -i "(inet\ )" | sed "s/netmask.*//g"| sed "s/inet//g" | sed "s/ //g")"
 }
 
+function get_os_release {
+
+#      Get os-release file, Only get field with "PRETTY_NAME" 2nd field, remove quotes.
+echo "$(cat /etc/os-release | awk -F= '$1=="PRETTY_NAME" {print $2; }' | tr -d "\"" )"
+
+}
 # Get the user's name.
 echo "First name:"
 read first_name
@@ -23,3 +29,6 @@ echo "You are currently logged in as '`whoami`'".
 # Get a list of this computer's registered IP addresses, and print them.
 echo "Here are your current IP addresses:"
 echo $(get_ips)
+
+echo "OS Release:"
+echo $(get_os_release)
