7327be1320230f2f95d41f20c343ae9575387a7e
Henry Post <HenryFBP@gmail.com>
2019-09-06 11:31:36 -0500

    start hw2

---
+++ homework/02/hw2.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+###
+# Henry Post, hpost@hawk.iit.edu
+#
+
+# Associative array with commands
+Options=(
+  "Exit"
+  "Ping a host"
+  "See hops to a host"
+  "Get a username from a UID"
+  "Zip a directory and send it via email"
+  "Send a file via email"
+)
+
+# Length of all commands they can enter
+OptionsLen=${#Options[@]}
+
+function echo_options() {
+  # Print digit: option pairs
+  for index in "${!Options[@]}"; do
+    echo "$index: ${Options[$index]}"
+  done
+
+  echo "Select an option (1-$(($OptionsLen - 1))) or 0 to exit."
+
+}
+
+INPUT="1"
+
+while [[ $INPUT != 0 ]]; do
+
+  echo_options
+
+  read INPUT
+
+  done
