2f3ffcc35147cf9a65b5bd88f3eec6204a204878
Henry Post <HenryFBP@gmail.com>
2019-09-06 21:37:44 -0500

    finish hw2

--- homework/02/hw2.sh
+++ homework/02/hw2.sh
@@ -6,9 +6,9 @@
 
 ONE_GIGABYTE_IN_KB=1048576
 
-TEMP_DIR="/tmp/ITMO417/hw2/"        # Place to store files temporarily for this shell script.
-TEMP_DIR_ZIP="$TEMP_DIR/zip/"       # Place to store zipped folders.
-TEMP_DIR_FOLDER="$TEMP_DIR/folder/" # Place to store folders.
+TEMP_DIR="/tmp/ITMO417/hw2/"  # Place to store files temporarily for this shell script.
+TEMP_DIR_ZIP="$TEMP_DIR/zip/" # Place to store zipped folders.
+TEMP_DIR_ZIP_FILE="${TEMP_DIR_ZIP}/archive.tar.gz"
 
 function clean_temp_dir() {
 
@@ -20,7 +20,6 @@ function clean_temp_dir() {
   # Make directories.
   mkdir -p ${TEMP_DIR}
   mkdir -p ${TEMP_DIR_ZIP}
-  mkdir -p ${TEMP_DIR_FOLDER}
 }
 
 # Associative array with commands
@@ -41,6 +40,34 @@ function echo_dir_size_in_kb() {
   du -s "$1" 2>/dev/null | awk '{print $1}'
 }
 
+# Convert KB to MB.
+function echo_kb_to_mb() {
+  echo $(($1 / 1000))
+}
+
+function send_file_by_email() {
+  echo "Enter the name of a file to send by email:"
+
+  echo_prompt
+  read filename
+
+  # Get absolute path.
+  filename=$(realpath "${filename}")
+
+  if ! [ -f "$filename" ]; then
+    echo "File '$filename' does not exist or is a directory!"
+    return
+  fi
+
+  echo "Enter email address:"
+
+  echo_prompt
+  read email
+
+  echo "Not emailing file $filename to $email as professor told us not to email in class."
+
+}
+
 function send_dir_by_email() {
   echo "Enter the path of a directory to zip and email:"
 
@@ -58,6 +85,12 @@ function send_dir_by_email() {
   dir_size_kb=$(echo_dir_size_in_kb "$directory")
 
   dir_size_gb=$((dir_size_kb / ONE_GIGABYTE_IN_KB))
+
+  echo "Directory at '${directory}' is:"
+  echo "${dir_size_kb}KB,"
+  echo "$(echo_kb_to_mb ${dir_size_kb})MB,"
+  echo "${dir_size_gb}GB."
+  sleep 3
 
   if ((dir_size_gb > 1)); then
 
@@ -77,10 +110,25 @@ function send_dir_by_email() {
     *)
       echo "Aborting."
       return
+      ;;
 
     esac
 
   fi
+
+  echo "Zipping '${directory}'..."
+  tar -zcvf "${TEMP_DIR_ZIP_FILE}" "${directory}"
+  echo "Done!"
+  echo "Archive is at '${TEMP_DIR_ZIP_FILE}'."
+
+  ls "${TEMP_DIR_ZIP_FILE}" -lash
+
+  echo "Enter email address:"
+
+  echo_prompt
+  read email
+
+  echo "Not emailing file ${directory} to ${email} as professor told us not to email in class."
 
 }
 
@@ -202,6 +250,10 @@ while [[ $INPUT != 0 ]]; do
 
     4)
       send_dir_by_email
+      ;;
+
+    5)
+      send_file_by_email
       ;;
 
     esac
