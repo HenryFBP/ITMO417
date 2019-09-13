14dcde11bc99322063bee30b05a0e4a80ca4b6fb
Henry Post <HenryFBP@gmail.com>
2019-09-13 11:41:49 -0500

    accept lots of args

--- homework/03/myrm.sh
+++ homework/03/myrm.sh
@@ -34,41 +34,60 @@ if [[ $# -lt 1 ]]; then
   exit 1
 fi
 
-# Absolute path to file to be recycled.
-absolute_garbage_path=$(readlink -f $1)
-absolute_garbage_path=$(echo_trim_slashes $absolute_garbage_path)
+# Given a path, handle recycling it.
+function handle_recycling_file() {
+  # Absolute path to file to be recycled.
+  absolute_garbage_path=$(readlink -f $1)
+  absolute_garbage_path=$(echo_trim_slashes $absolute_garbage_path)
 
-if [[ ! -e $absolute_garbage_path ]]; then
-  echo "File at ${absolute_garbage_path} does not exist!"
-  exit 1
-fi
+  if [[ ! -e $absolute_garbage_path ]]; then
+    echo "File at ${absolute_garbage_path} does not exist!"
+    exit 1
+  fi
 
-# A unique name for the file or folder being recycled.
-unique_name=$(echo_unique_name "$(basename "$1")")
-recycled_filepath="${WASTEBASKET_DIR}${unique_name}"
+  # A unique name for the file or folder being recycled.
+  unique_name=$(echo_unique_name "$(basename "$1")")
+  recycled_filepath="${WASTEBASKET_DIR}${unique_name}"
 
-echo "About to perform the following move operation:"
+  echo "About to perform the following move operation:"
 
-echo "[  SRC  ] --> $absolute_garbage_path"
-echo "[  DEST ] --> $recycled_filepath"
-echo "[  SIZE ]  :  $(du -sh ${absolute_garbage_path} | cut -f1)"
+  # Make sure they know what they're doing...
+  echo "[  SRC  ] --> $absolute_garbage_path"
+  echo "[  DEST ] --> $recycled_filepath"
+  echo "[  SIZE ]  :  $(du -sh ${absolute_garbage_path} | cut -f1)"
 
-read -p "Is this move operation okay? (y/n)
+  # Ask them for confirmation
+  read -p "Is this move operation okay? (y/n)
  > " INPUT
 
-echo $INPUT
+  echo $INPUT
 
-case $INPUT in
+  case $INPUT in
 
-y | Y)
-  echo "YES"
-  ;;
+  y | Y)
+    echo "Proceeding."
+    ;;
 
-n | N)
-  echo "NOPE"
-  ;;
+  n | N)
+    echo "Aborting!"
+    return 0
+    ;;
 
-*)
-  echo "lol what"
-  ;;
-esac
+  *)
+    echo "Unknown response. Aborting!"
+    return 1
+    ;;
+  esac
+
+  mv ${absolute_garbage_path} ${recycled_filepath}
+
+  echo "Your files are at '${recycled_filepath}'."
+
+}
+
+# For all arguments,
+for filepath in "$@"; do
+  # Try to recycle their argument.
+  handle_recycling_file "${filepath}"
+done
+
