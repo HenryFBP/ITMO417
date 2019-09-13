b3191d1659e924fa4f1836f8680bd3cf184473a8
Henry Post <HenryFBP@gmail.com>
2019-09-13 11:25:52 -0500

    generate realistic crap

--- homework/03/generate-junk.sh
+++ homework/03/generate-junk.sh
@@ -1,18 +1,22 @@
 #!/usr/bin/env bash
 
+function fill_file_with_random_data() {
+    head -c 20M </dev/urandom > $1
+}
+
 mkdir -p junk
 pushd junk
 
 mkdir -p img
 pushd img
-touch cool_image.jpg
-touch cat.gif
+fill_file_with_random_data cool_image.jpg
+fill_file_with_random_data cat.gif
 popd
 
 mkdir -p data
 pushd data
-touch names.txt
-touch hard_drive.img
+fill_file_with_random_data names.txt
+fill_file_with_random_data hard_drive.img
 popd
 
 
