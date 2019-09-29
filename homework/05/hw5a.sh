94075d59af424110d1eeb3567ab3424ca509f322
Henry Post <HenryFBP@gmail.com>
2019-09-29 00:15:04 -0500

    redo hw5a 1-3

--- homework/05/hw5a.sh
+++ homework/05/hw5a.sh
@@ -9,18 +9,19 @@ function newline() {
     echo ''
 }
 
-# The homework says to use datafile, but it should be datebook.
-FILENAME=datebook.txt
+FILENAME=grepDatafile.txt
 
-echo "HW5A 1: Phone numbers"
-grep -P '\d{3}-\d{3}-\d{4}' "$FILENAME"
+echo "HW5A 1: Phone numbers with extensions"
+grep -P '\d{3}-\d{3}-\d{4} [xX]\d{4}' "$FILENAME"
 newline
 
 echo "HW5A 2: 3 digits followed by a blank"
-grep -P ":\d\d\d\ " "$FILENAME"
+grep -P "\d{3} " "$FILENAME"
 newline
 
 echo "HW5A 3: Lines with a date"
-grep -P ":\d{1,2}/\d{1,2}/\d{1,2}" "$FILENAME"
+grep -P "[a-zA-Z]{3}\. \d{1,2}, \d{4}" "$FILENAME"
 newline
 
+echo "HW5A 4: Vowels followed by a character with capturing groups"
+grep -P "potato :)" "$FILENAME"
