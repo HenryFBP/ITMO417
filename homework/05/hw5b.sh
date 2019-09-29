a1d15bb2b7e73444e15f2bcf13230254a65afa70
Henry Post <HenryFBP@gmail.com>
2019-09-29 16:31:46 -0500

    show off demo of hw5b, finish it

--- homework/05/hw5b.sh
+++ homework/05/hw5b.sh
@@ -9,6 +9,11 @@ function newline() {
     echo ''
 }
 
+if ! hash egrep 2>/dev/null; then
+    echo "Install 'egrep'!";
+    exit 1
+fi
+
 FILENAME=sedDatafile.txt
 OUT_DIR="out/"
 if [[ -d ${OUT_DIR} ]]; then
@@ -30,3 +35,39 @@ sed -E "s/^([a-zA-Z]+) ([a-zA-Z]+):(.*)$/\2 \1:\3/"  ${FILENAME} > ${OUT_DIR}/3.
 
 # 4. Append at the end of the file THE END
 sed -e "\$aTHE END" ${FILENAME} > ${OUT_DIR}/4.txt
+
+echo "# 1. Insert above the first line the title PERSONNEL FILE"
+echo "Orig":
+head -1 ${FILENAME}
+newline
+
+echo "Modified":
+head -1 ${OUT_DIR}/1.txt
+newline
+
+echo "# 2. Remove the salaries ending in 500"
+echo "Orig":
+tail -10 ${FILENAME} | egrep --color '500|'
+newline
+
+echo "Modified":
+tail -10 ${OUT_DIR}/2.txt
+newline
+
+echo "# 3. Print the contents of the file with the last name and first names reversed"
+echo "Orig":
+head -5 ${FILENAME}
+newline
+
+echo "Modified":
+head -5 ${OUT_DIR}/3.txt
+newline
+
+echo "# 4. Append at the end of the file THE END"
+echo "Orig":
+tail -1 ${FILENAME}
+newline
+
+echo "Modified":
+tail -1 ${OUT_DIR}/4.txt
+newline
