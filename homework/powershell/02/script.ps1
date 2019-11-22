59bb0de040ecb23935def2e36d45334e03b741d8
Henry Post <HenryFBP@gmail.com>
2019-11-22 00:43:22 -0600

    execute anon fn

--- homework/powershell/02/script.ps1
+++ homework/powershell/02/script.ps1
@@ -1,17 +1,22 @@
-function Show-Menu
+function Run-Menu
 {
     param (
         [string]$Title = 'My Menu',
-        [string[]]$options = @('Item 1', 'Item Two')
+        [string[]]$options = @('Item 1', 'Item Two'),
+        [Object[]]$callbacks = @({ Write-Host "You chose item one." }, { Write-Host "You chose item 2!" })
     )
     Clear-Host
     Write-Host "================ $Title ================"
 
     for ($i = 0; $i -lt $options.Count; $i++) {
         Write-Host "$i)" $options[$i]
+        & $callbacks[$i] # This executes the lambda fn
     }
 
     Write-Host "Q: Press 'Q' to quit."
 }
 
-Show-Menu -Title "potato" -options "asdf","asdFASDFAFD"
+
+$options = @()
+
+Run-Menu -Title "potato"
