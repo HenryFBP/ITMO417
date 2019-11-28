5e202bfc1c179e260e4a149484b194e80428af84
Henry Post <HenryFBP@gmail.com>
2019-11-27 20:11:12 -0600

    start q2

---
+++ homework/final/question-2.pl
@@ -0,0 +1,42 @@
+#!/usr/bin/perl
+use strict;
+use warnings FATAL => 'all';
+
+print "Henry Post, hpost\@hawk.iit.edu, ITMO 417 at IIT.\n";
+
+=head3 subname
+
+Question 1 (PowerShell Scripting)
+
+Given a PowerShell program shown below, finish the program by completing the function named findOddCount().
+The function finds a value in the given list that occurs an odd number of times.
+
+$a1 = (1,1,2,2,3,3,4,4,5,5,6,7,7,7,7)
+$a2 = (10,10,7,7,6,6, 2,2,3,3,4,4,5,5,6,7,7,7,7,10,10)
+$a3 = (6,6,10,10,7,7,6,6, 2,2,3,3,4,4,5,5,6,7,7,7,7,10,10)
+$a4 = (10,10,7,7, 2,2,3,3,4,4,5,5,7,7,7,7,10,10,6)
+$a5 = (6,6)
+$a6 = (1)
+
+Write-Host -noNewLine "odd value in a1 is "; findOddCount($a1)
+Write-Host -noNewLine "odd value in a2 is "; findOddCount($a2)
+Write-Host -noNewLine "odd value in a3 is "; findOddCount($a3)
+Write-Host -noNewLine "odd value in a4 is "; findOddCount($a4)
+Write-Host -noNewLine "odd value in a5 is "; findOddCount($a5)
+Write-Host -noNewLine "odd value in a6 is "; findOddCount($a6)
+
+=cut
+
+
+my @a1 = (1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 7, 7, 7, 7);
+my @a2 = (10, 10, 7, 7, 6, 6, 2, 2, 3, 3, 4, 4, 5, 5, 6, 7, 7, 7, 7, 10, 10);
+my @a3 = (6, 6, 10, 10, 7, 7, 6, 6, 2, 2, 3, 3, 4, 4, 5, 5, 6, 7, 7, 7, 7, 10, 10);
+my @a4 = (10, 10, 7, 7, 2, 2, 3, 3, 4, 4, 5, 5, 7, 7, 7, 7, 10, 10, 6);
+my @a5 = (6, 6);
+my @a6 = (1);
+
+my @arrays = (\@a1, \@a2, \@a3, \@a4, \@a5, \@a6);
+
+foreach my $arr (@arrays) {
+    print($arr . "\n");
+}
