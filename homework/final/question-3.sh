49d7a237f73dc1bec922738622be0059a5c6606d
Henry Post <HenryFBP@gmail.com>
2019-11-28 19:19:08 -0500

    finish q3

--- homework/final/question-3.sh
+++ homework/final/question-3.sh
@@ -27,24 +27,47 @@ a5=(6 6);
 a6=(1);
 
 function echo_odd_count() {
-    echo "$@"
-    echo "test"
+#    echo "$@"
+#    echo "test"
 
     declare -A "numbers_occurrences"
 
     # count up how many times each number occurs and store it in $numbers_occurrences
     for number in "$@" ; do
-        echo ${number}
+#        echo ${number}
 
         # if we've seen this number before,
-        if test "${numbers_occurrences[number]+isset}"; then
-            true
-        else
-            true
+        if test "${numbers_occurrences[$number]+isset}"; then
+            # add 1
+            numbers_occurrences[$number]=$((${numbers_occurrences[$number]} + 1))
+        else # We haven't seen it before, except now, which is once.
+            numbers_occurrences[$number]=1
         fi
-
     done
+
+    # Find all the odd number occurrences
+    declare -A odd_numbers_occurrences;
+
+    for number in "${!numbers_occurrences[@]}"; do
+        if ((${numbers_occurrences[$number]} % 2)); then # if number occurrences are odd,
+            odd_numbers_occurrences[$number]=${numbers_occurrences[$number]}
+        fi
+    done
+
+    echo "For the array of '$@':"
+    for number in "${!odd_numbers_occurrences[@]}"; do
+        occurrences=${odd_numbers_occurrences[$number]}
+
+        echo "$number occurs $occurrences times."
+    done
+
+
 
 }
 
 echo_odd_count "${a1[@]}"
+echo_odd_count "${a2[@]}"
+echo_odd_count "${a3[@]}"
+echo_odd_count "${a4[@]}"
+echo_odd_count "${a5[@]}"
+echo_odd_count "${a6[@]}"
